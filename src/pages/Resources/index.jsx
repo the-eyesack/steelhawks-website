d9d55b6e8541c69fee6f6fba67849e5ef12afe85
Owen Shi <2008owenshi@gmail.com>
2023-01-27 08:27:38 -0500

    resources title, remove some data

--- src/pages/Resources/index.jsx
+++ src/pages/Resources/index.jsx
@@ -3,29 +3,32 @@ import { resourcesData } from './data';
 
 function ResourcesPage() {
   return (
-    <div className='grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 p-10 gap-y-10 gap-x-4'>
-      {resourcesData.map((section) => (
-        <div>
-          <h1 className='text-4xl'>{section.title}</h1>
-          {section.topics.map((topic) => (
-            <div>
-              <li className='text-2xl mt-2 ml-6'>{topic.title}</li>
-              {topic.resources.map((resources) => (
-                <li className='ml-12'>
-                  <a
-                    className='hover:underline duration-300'
-                    href={resources.link}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    {resources.title}
-                  </a>
-                </li>
-              ))}
-            </div>
-          ))}
-        </div>
-      ))}
+    <div className='px-10 pb-10'>
+      <h1 className='pageTitle'>Resources</h1>
+      <div className='grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-y-10 gap-x-4'>
+        {resourcesData.map((section) => (
+          <div>
+            <h1 className='text-4xl'>{section.title}</h1>
+            {section.topics.map((topic) => (
+              <div>
+                <li className='text-2xl mt-2 ml-6'>{topic.title}</li>
+                {topic.resources.map((resources) => (
+                  <li className='ml-12'>
+                    <a
+                      className='hover:underline duration-300'
+                      href={resources.link}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
+                      {resources.title}
+                    </a>
+                  </li>
+                ))}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
